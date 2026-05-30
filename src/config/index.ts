35a06e20277045337b4a1362086dc805156fb9ef
Mahbub Hasan <easycodermahbubwith5500@gmail.com>
2026-05-30 18:17:53 +0600

    database connected successfully

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,6 @@ import path from "path";
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export const config = {
+  connecting_string: process.env.CONNECTINGSTRING,
   port: process.env.PORT,
 };
---
+++ src/db/index.ts
@@ -0,0 +1,29 @@
+import { Pool } from "pg";
+import { config } from "../config";
+
+export const pool = new Pool({
+  connectionString: config.connecting_string,
+});
+
+const initDB = async () => {
+  try {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20) NOT NULL,
+        email VARCHAR(30) UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        roll VARCHAR(20) DEFAULT 'shop owener',
+        is_active BOOLEAN DEFAULT FALSE,
+
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default initDB;
